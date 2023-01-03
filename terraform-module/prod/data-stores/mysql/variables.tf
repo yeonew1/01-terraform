a011151ecc4fe1cde30daf49c4baa15107e57da2
yeonew1 <990.ginger@gmail.com>
2023-01-03 15:44:00 +0900

    20230103

---
+++ terraform-module/prod/data-stores/mysql/variables.tf
@@ -0,0 +1,17 @@
+variable "db_username" {
+  description = "The password for the database"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "The password for the database"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "The name to use for the database"
+  type        = string
+  default     = "aws03_example_database_prod"
+}
